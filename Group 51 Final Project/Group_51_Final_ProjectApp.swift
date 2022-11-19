fa2bc2529fb8ca206474e6dcc6ac8f7844865a23
austinbasu <72290934+austinbasu@users.noreply.github.com>
2022-11-18 18:59:10 -0700

    Initial Commit

---
+++ Group 51 Final Project/Group_51_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_51_Final_ProjectApp.swift
+//  Group 51 Final Project
+//
+//  Created by Austin Buford on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Group_51_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
