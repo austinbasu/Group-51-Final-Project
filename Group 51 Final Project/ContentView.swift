1e721f3a81ef31a352c99772db29f662121f3f0a
austinbasu <72290934+austinbasu@users.noreply.github.com>
2022-11-18 19:02:59 -0700

    Initial Commit

--- Group 51 Final Project/ContentView.swift
+++ Group 51 Final Project/ContentView.swift
@@ -1,21 +1,41 @@
-//
-//  ContentView.swift
-//  Group 51 Final Project
-//
-//  Created by Austin Buford on 11/18/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack{
+                LinearGradient(colors: [.indigo, .teal], startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+                VStack {
+                    Spacer()
+                    NavigationLink(destination: RecommendationView(), label: {
+                        Text("Movie Recommendation")
+                            .frame(width: 200, height: 50, alignment: .center)
+                            .background(Color.white)
+                            .foregroundColor(.blue)
+                            .clipShape(Capsule())
+                            .padding(.all)
+                            .navigationTitle("Dashboard")
+                    }).offset(y: -75)
+                    NavigationLink(destination: RecommendationView(), label: {
+                        Text("Movie List")
+                            .frame(width: 200, height: 50, alignment: .center)
+                            .background(Color.white)
+                            .foregroundColor(.blue)
+                            .clipShape(Capsule())
+                            .padding(.all)
+                    }).offset(y: -50)
+                    NavigationLink(destination: RecommendationView(), label: {
+                        Text("Watchlist")
+                            .frame(width: 200, height: 50, alignment: .center)
+                            .background(Color.white)
+                            .foregroundColor(.blue)
+                            .clipShape(Capsule())
+                            .padding(.all)
+                    }).offset(y: -25)
+                    Spacer()
+                }
+            }
+        }.accentColor(.black)
     }
 }
 
---
+++ Group 51 Final Project/RecommendationView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct RecommendationView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.indigo, .teal], startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Movie Recommendation")
+                        .offset(y: -350)
+                        .font(.system(size: 24))
+                }
+            }
+        }
+    }
+}
+
+struct RecommendationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecommendationView()
+    }
+}
