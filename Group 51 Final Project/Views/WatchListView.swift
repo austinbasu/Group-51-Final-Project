5f342b306fc762b8a1a5f07b4d285d569fcf2b7d
jacob5234 <jmong@asu.edu>
2022-11-26 21:19:28 -0700

    adding watchlist changes
    joey will finish it when he gets back

--- Group 51 Final Project/Views/WatchListView.swift
+++ Group 51 Final Project/Views/WatchListView.swift
@@ -1,17 +1,53 @@
 import SwiftUI
 
 struct WatchListView: View {
+    @ObservedObject var movieModel = MovieModel()
     var body: some View {
         NavigationView {
-            ZStack {
-                LinearGradient(colors: [.indigo, .teal], startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
-                VStack {
-                    Text("Watch List")
-                        .offset(y: -350)
-                        .font(.system(size: 24))
+            
+            VStack(alignment: .center) {
+                List {
+                    ForEach(movieModel.movieList, id: \.self) { movie in
+                        Text("\(movie.movieTitle!)")
+                        Text("\(movie.rating!)")
+                        NavigationLink("View Movie", destination:DetailView(title: movie.movieTitle, descr: movie.description, rating: movie.rating, releaseDate: movie.releaseDate))
+                    }
                 }
             }
+        .navigationTitle("My Watched Movies").navigationBarTitleDisplayMode(.inline)
+            .toolbar
+        {
+            /*ToolbarItemGroup(placement: .navigationBarLeading)
+            {
+                NavigationLink(destination: RemoveCityView(customView: viewModel))
+                {
+                    Label("Remove", systemImage: "minus.square")
+                }
+            }*/
+                
         }
+        .toolbar
+        {
+            ToolbarItemGroup(placement: .navigationBarTrailing)
+            {
+                NavigationLink(destination: AddMovieView(movieModel: movieModel))
+            {
+                Label("Add", systemImage: "plus.app")
+            }
+            }
+        }
+        }
+            
+            
+//            ZStack {
+//                LinearGradient(colors: [.indigo, .teal], startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+//                VStack {
+//                    Text("Watch List")
+//                        .offset(y: -350)
+//                        .font(.system(size: 24))
+//                }
+//            }
+       // }
     }
 }
 
